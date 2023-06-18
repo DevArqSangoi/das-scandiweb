4bc121f292aef234ddf7fe22f911a8728d1f8e0b
DevArqSangoi <lucas.sangoi@gmail.com>
2023-06-18 18:15:22 -0300

    Build

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,9 +1,9 @@
 <?php
-require_once __DIR__ . '/../src/Database/ProductsDatabase.php';
-require_once __DIR__ . '/../src/Product/ProductAPI.php';
-require_once __DIR__ . '/../src/Product/DVD.php';
-require_once __DIR__ . '/../src/Product/Book.php';
-require_once __DIR__ . '/../src/Product/Furniture.php';
+require_once __DIR__ . '/src/Database/ProductsDatabase.php';
+require_once __DIR__ . '/src/Product/ProductAPI.php';
+require_once __DIR__ . '/src/Product/DVD.php';
+require_once __DIR__ . '/src/Product/Book.php';
+require_once __DIR__ . '/src/Product/Furniture.php';
 require_once 'router.php';
 
 $api = new ProductAPI();
