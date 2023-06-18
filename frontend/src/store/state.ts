28c312134d6fe25725926e9f45b8d6d3e49d2829
Lucas Freire Sangoi <125471877+DevArqSangoi@users.noreply.github.com>
2023-06-18 18:23:16 -0300

    Delete frontend directory

--- frontend/src/store/state.ts
+++
@@ -1,7 +0,0 @@
-// src/store/state.ts
-console.log("Executando state.ts...");
-import { Product } from "./productsModule";
-export interface RootState {
-  products: Product[];
-}
-console.log("Finalizado state.ts");
